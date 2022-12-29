7e856c79e0c57f3b7559fb4325c29b83100b77d5
randy.lopez <randy.lopez@semarnat.gob.mx>
2022-12-28 19:37:15 -0600

    Se crean los modelos 'AdoptionForm' y 'AdoptionRequest' para crear el formulario de adopcion y la solicitud de adopcion

--- apps/pet/admin.py
+++ apps/pet/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from apps.pet.models import Pet, PetImage
+from apps.pet.models import Pet, PetImage, AdoptionForm, AdoptionRequest
 
 # Register your models here.
 
 admin.site.register(Pet)
 admin.site.register(PetImage)
+admin.site.register(AdoptionForm)
+admin.site.register(AdoptionRequest)
--- apps/pet/models.py
+++ apps/pet/models.py
@@ -32,13 +32,14 @@ class Pet(AbstractBaseModel):
     size = models.CharField(max_length = 2, choices = SIZE_CHOICES)
     type_of_pet = models.CharField(max_length = 1, choices = TYPE_OF_PET_CHOICES)
     description = models.TextField()
+    status = models.BooleanField(default = True)
     
     def __str__(self):
         return self.name
 
 class PetImage(AbstractBaseModel):
     uuid = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
-    pet = models.ForeignKey(Pet, on_delete = models.CASCADE)
+    pet = models.ForeignKey(Pet, on_delete = models.CASCADE, related_name = 'pet_images')
     image = models.ImageField(upload_to = generate_path_upload_images,
                             null = True, 
                             blank = True,
@@ -64,3 +65,34 @@ class PetImage(AbstractBaseModel):
         if old_image and old_image != self.image:
             old_image.storage.delete(old_image.name)
 
+class AdoptionForm(AbstractBaseModel):
+    uuid = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    name = models.CharField(max_length = 100)
+    last_name = models.CharField(max_length = 100)
+    second_last_name = models.CharField(max_length = 100, null = True, blank = True)
+    email = models.EmailField()
+    phone_number = models.CharField(validators = [validate_phone], max_length = 12)
+    country = models.CharField(max_length = 100, default = "México")
+    city = models.CharField(max_length = 100)
+    state = models.CharField(max_length = 100)
+    town = models.CharField(max_length = 100)
+    postal_code = models.CharField(max_length = 5)
+    address = models.CharField(max_length = 250)
+    message = models.TextField()
+
+    def __str__(self):
+        return f'Formulario de: {self.name}'
+    
+class AdoptionRequest(AbstractBaseModel):
+    STATUS_CHOICES = [
+        ('S', 'Sent'),
+        ('A', 'Approved'),
+        ('R', 'Rejected')
+    ]
+    uuid = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    pet = models.ForeignKey(Pet, on_delete = models.CASCADE, related_name = 'pet_adoption_requests')
+    adoption_form = models.ForeignKey(AdoptionForm, on_delete = models.CASCADE, related_name = 'form_adoption_requests')
+    status = models.CharField(max_length = 1, choices = STATUS_CHOICES)
+    
+    def __str__(self):
+        return f'Solicitud de adopción para: {self.pet.name}'
